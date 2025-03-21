ac5f1938ba24a0c3b52d23b6ac6ee1283af1a6f7
Martin <martin.allenstein@gmail.com>
2025-03-22 00:47:49 +0700

    Add Collider, Add Physics

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField]private float speed = 5f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,22 +14,22 @@ public class PlayerController : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.W))
         {
-            transform.Translate(Vector3.forward * Time.deltaTime * 10f);
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
         }
 
         if (Input.GetKey(KeyCode.S))
         {
-            transform.Translate(Vector3.back * Time.deltaTime * 10f);
+            transform.Translate(Vector3.back * Time.deltaTime * speed);
         }
 
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Translate(Vector3.left * Time.deltaTime * 10f);
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
         }
 
         if (Input.GetKey(KeyCode.D))
         {
-            transform.Translate(Vector3.right * Time.deltaTime * 10f);
+            transform.Translate(Vector3.right * Time.deltaTime * speed);
         }
     }
 }
